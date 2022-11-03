65efd8138b885b1d7a7d53fda0b644e52666bd10
Prapti Shadmaan <praptishadmaan@gmail.com>
2022-11-03 14:00:04 -0700

    Draw arrow when a highlighted block is outside minimap

--- dis-viz-frontend/src/components/Minimap.tsx
+++ dis-viz-frontend/src/components/Minimap.tsx
@@ -10,46 +10,75 @@ const BLOCK_SEP = 5
 const BLOCK_LINE_LEFT = 20
 const BLOCKS_START_TOP = 50
 const BRUSH_OFFSET = 10
+const HIDDEN_ARROW_GAP = 30
+const HIDDEN_ARROW_LEN = 20
+
+function canvas_arrow(context: CanvasRenderingContext2D, fromx: number, fromy: number, tox: number, toy: number) {
+  var headlen = 10; // length of head in pixels
+  var dx = tox - fromx;
+  var dy = toy - fromy;
+  var angle = Math.atan2(dy, dx);
+  context.moveTo(fromx, fromy);
+  context.lineTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+  context.moveTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+}
+
 
 export default function Minimap({ visibleBlockWindow, width, ...props }: {
     width: number,
     visibleBlockWindow: {start: number, end: number}
 }) {
+    const canvasRef = React.useRef<HTMLCanvasElement>(null)
     const minimap = useAppSelector(selectMinimap)
     const selections = useAppSelector(selectSelections)
     const totalBlocks = minimap.blockStartAddress.length // b
     const brushStartBlockI = minimap.blockStartAddress.findIndex(address => address === visibleBlockWindow.start)
     const brushEndBlockI = minimap.blockStartAddress.findIndex(address => address === visibleBlockWindow.end)
 
-    console.log("Scroll Percent: ", brushStartBlockI/totalBlocks)
+    let drawingStartBlockI:number = brushStartBlockI, drawingEndBlockI:number = brushStartBlockI+1
+    
+    const height = canvasRef.current?canvasRef.current.height:window.innerHeight
 
-    const canvasRef = React.useRef<HTMLCanvasElement>(null)
+    {
+        let topHeight: number = brushStartBlockI/totalBlocks * height - BLOCKS_START_TOP - BRUSH_OFFSET - BLOCK_SEP
+        while(topHeight > 100) {
+            drawingStartBlockI -= 1
+            if(drawingStartBlockI < 0) {
+                drawingStartBlockI = 0
+                break
+            }
+            topHeight -= minimap.blockHeights[drawingStartBlockI]*BLOCK_LINE_HEIGHT_FACTOR + BLOCK_SEP
+        }
+        let bottomHeight: number = height - brushStartBlockI/totalBlocks * height
+        while(bottomHeight > -100) {
+            drawingEndBlockI += 1
+            if(drawingEndBlockI >= totalBlocks) {
+                drawingEndBlockI = totalBlocks - 1
+                break
+            }
+            bottomHeight -= minimap.blockHeights[drawingEndBlockI]*BLOCK_LINE_HEIGHT_FACTOR + BLOCK_SEP
+        }
+    }
+
+    const topHidden: number[] = [], bottomHidden: number[] = []
+    for(const disViewId in selections) {
+        const selection = selections[disViewId]
+        if(selection) {
+            if(selection.addresses.some(address => address < minimap.blockStartAddress[drawingStartBlockI])) {
+                topHidden.push(parseInt(disViewId))
+            }
+            if(selection.addresses.some(address => address > minimap.blockStartAddress[drawingEndBlockI])) {
+                bottomHidden.push(parseInt(disViewId))
+            }
+        }
+    }
+
+
 
     const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
         let brushStartY: number|null = null, brushEndY: number|null = null
-        let drawingStartBlockI:number = brushStartBlockI, drawingEndBlockI:number = brushStartBlockI+1
-        
-        {
-            let topHeight: number = brushStartBlockI/totalBlocks * ctx.canvas.height - BLOCKS_START_TOP - BRUSH_OFFSET - BLOCK_SEP
-            while(topHeight > 100) {
-                drawingStartBlockI -= 1
-                if(drawingStartBlockI < 0) {
-                    drawingStartBlockI = 0
-                    break
-                }
-                topHeight -= minimap.blockHeights[drawingStartBlockI]*BLOCK_LINE_HEIGHT_FACTOR + BLOCK_SEP
-            }
-            let bottomHeight: number = ctx.canvas.height - brushStartBlockI/totalBlocks * ctx.canvas.height
-            while(bottomHeight > -100) {
-                drawingEndBlockI += 1
-                if(drawingEndBlockI >= totalBlocks) {
-                    drawingEndBlockI = totalBlocks - 1
-                    break
-                }
-                bottomHeight -= minimap.blockHeights[drawingEndBlockI]*BLOCK_LINE_HEIGHT_FACTOR + BLOCK_SEP
-            }
-        }
-
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
         ctx.fillStyle = "#FFFFFF"
 
@@ -105,6 +134,35 @@ export default function Minimap({ visibleBlockWindow, width, ...props }: {
             BLOCK_LINE_LEFT+BLOCK_LINE_WIDTH+BRUSH_OFFSET,
             brushEndY! - brushStartY!
         );
+
+        // ctx.font = '32px serif';
+        // ctx.fillText(
+        //     Math.ceil(brushStartBlockI/totalBlocks * 100).toString(),
+        //     BLOCK_LINE_LEFT + BLOCK_LINE_WIDTH / 2,
+        //     (brushStartY! + brushEndY!)/2
+        // )
+
+        // Draw the hidden marked blocks
+        topHidden.forEach((disViewId, i) => {
+            ctx.beginPath()
+            ctx.strokeStyle = codeColors[disViewId]
+            canvas_arrow(ctx,
+                BLOCK_LINE_LEFT + BLOCK_LINE_WIDTH/20 + i*HIDDEN_ARROW_GAP,
+                BLOCKS_START_TOP,
+                BLOCK_LINE_LEFT + BLOCK_LINE_WIDTH/20 + i*HIDDEN_ARROW_GAP,
+                BLOCKS_START_TOP - HIDDEN_ARROW_LEN)
+            ctx.stroke()
+        })
+        bottomHidden.forEach((disViewId, i) => {
+            ctx.beginPath()
+            ctx.strokeStyle = codeColors[disViewId]
+            canvas_arrow(ctx,
+                BLOCK_LINE_LEFT + BLOCK_LINE_WIDTH/20 + i*HIDDEN_ARROW_GAP,
+                ctx.canvas.height - HIDDEN_ARROW_LEN,
+                BLOCK_LINE_LEFT + BLOCK_LINE_WIDTH/20 + i*HIDDEN_ARROW_GAP,
+                ctx.canvas.height)
+            ctx.stroke()
+        })
     }
 
     React.useEffect(() => {
