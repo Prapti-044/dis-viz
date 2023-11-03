6b9e89892b4ea0df37cc2c8b88f0256026fd1179
Shadmaan Hye <praptishadmaan@gmail.com>
2023-11-03 14:25:40 -0600

    Fixed bug in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/dis-viz-backend
+cd "$SCRIPT_DIR/dis-viz-backend"
 mkdir -p build
 cd build
 cmake ..
