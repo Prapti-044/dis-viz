cfe627caee2659e912cdbd9bdce98e884a2400c1
Shadmaan Hye Prapti <praptishadmaan@gmail.com>
2023-11-03 13:31:09 -0600

    Fixed dockerfile

--- dis-viz-frontend/src/config.ts
+++ dis-viz-frontend/src/config.ts
@@ -1,5 +1,5 @@
 export function getUrls() {
-  const host_url = window.location.protocol + "//" + window.location.host.split(':')[0] + ':' + process.env.REACT_APP_BACKEND_PORT;
+  const host_url = window.location.protocol + "//" + window.location.host;
   
   return {
     backend: host_url
