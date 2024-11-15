16bc7acfda93d3fd3e0cb7829d05652ac3bc8b42
Prapti-044 <praptishadmaan@gmail.com>
2024-11-14 22:24:01 -0700

    codehoist example

--- sample_inputs/compile.sh
+++ sample_inputs/compile.sh
@@ -3,6 +3,11 @@
 g++ -g -O0 hello.cpp -o bin/hello-O0
 g++ -g -O3 hello.cpp -o bin/hello-O3
 
+gcc -g -O0 codehoist.c -o bin/codehoist-O0
+gcc -g -O1 codehoist.c -o bin/codehoist-O1
+gcc -g -O2 codehoist.c -o bin/codehoist-O2
+gcc -g -O3 codehoist.c -o bin/codehoist-O3
+
 g++ -g -O0 bubble_sort.cpp -o bin/bubble-O0
 g++ -g -O1 bubble_sort.cpp -o bin/bubble-O1
 g++ -g -O2 bubble_sort.cpp -o bin/bubble-O2
@@ -20,9 +25,9 @@ gcc -g -O3 multisource/hello.c -o bin/multisource-O3
 gcc -g -O0 ifelse.c -o bin/ifelse-O0
 gcc -g -O3 ifelse.c -o bin/ifelse-O3
 
-nvcc -g -G -O0 vec_add.cu -o bin/vec_add-O0
-nvcc -g -G -O3 vec_add.cu -o bin/vec_add-O3
-
 g++ -g -O0 inlines/file2.cpp -o bin/inlines-O0
 g++ -g -O3 inlines/file2.cpp -o bin/inlines-O3
+
+nvcc -g -G -O0 vec_add.cu -o bin/vec_add-O0
+nvcc -g -G -O3 vec_add.cu -o bin/vec_add-O3
 
